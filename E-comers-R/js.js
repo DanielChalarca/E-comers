dc3f904f09e080663f2027fbad75e68d1b03ae4e
DanielChalarca <danielchalarcach@gmail.com>
2024-10-07 15:28:59 -0500

    Add files via upload

---
+++ E-comers-R/js.js
@@ -0,0 +1,129 @@
+// var metros2= 20;
+// var kilometros = metros2 / 100;
+// var centimetros = metros2 * 100;
+// console.log(kilometros)
+// console.log(centimetros) 
+
+// function conversorDistancia(metros){
+//   var kilometro = metros / 1000;
+//   var centimetro = metros * 100;
+//   var pulgada = metros * 39.3701;
+//   var pie = metros * 3.28;
+//   var decametro = metros / 10;
+  
+//   return{
+//     metros : metros,
+//     kilometros: kilometro,
+//     decametros: decametro,
+//     centimetros: centimetro,
+//     pulgadas: pulgada,
+//     pies:pie
+//   };
+   
+// }
+// var resultados = conversorDistancia(metros2);
+// console.log("valor en metros: " + resultados.metros)
+// console.log("valor en kilometros: " + resultados.kilometros)
+// console.log("valor en decametros: " + resultados.decametros)
+// console.log("valor en centimetros: " + resultados.centimetros)
+// console.log("valor en pulgadas: " + resultados.pulgadas)
+// console.log("valor en pies: " + resultados.pies)
+
+// function conversor2(valor, uniDesde, unidadHacia) {
+//   const factores ={
+//     metros: 1,
+//     kilometros: 0.001,
+//     decametros: 0.1,
+//   }
+//   if(factores.hasOwnProperty(uniDesde))
+-----------------------------------------------------------------------------
+/* inicio */
+
+/* var newVariable = "new code now";
+
+var miNombre = "Daniel";
+
+console.log(miNombre) */
+
+/*
+var a = 5 ;
+var b = a ;
+ console.log( b )
+*/
+
+/*
+var x = 9 ; // Inicializar la variable
+
+var miIdiama = "spanish"; //(miIdioma) nomenclatura: -Camelcase
+
+*/
+
+/*
+//variables no inicializadas
+
+var a;
+var b;
+
+a = 3 ;
+b = "c";
+  
+  console.log(a)
+
+*/
+
+/*
+//Suma 
+;lkm
+var suma = 7 + 7;
+
+console.log(suma)
+
+*/
+
+/*
+//Resta
+
+ var a = 5;
+ var b = 15;
+
+var resta = b - a;
+
+console.log(resta)
+
+*/
+
+
+// funtion suma(array) {
+//   let suma = 0;
+//   for ( let i = 0; array. length; i++ ){
+//     suma += array[i];
+//     suma = suma + array [i]
+
+//   }
+//   return suma;
+// }
+
+// const numeros = [ 1 , 2 , 3 , 4 , 5];
+
+// console.log(suma(numeros)); 
+
+
+
+
+funtion suma (array) {
+  let suma = 0, pares 0;
+  for ( let i = 0; i < array.length; i++ ){
+    if ( array [i]%2 == 0){
+      suma += array[i];
+      pares += 1;
+    }
+
+  }
+  let resultado = [suma, pares]
+  return resultados;
+}
+
+const numeros [1, 2, 3, 4, 5, 6, 7, 8 ,9];
+console.log (suma(numeros));
+
+funtion 
